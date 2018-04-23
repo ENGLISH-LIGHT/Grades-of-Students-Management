cf00d9b6e1fb8d6fbadedc1f7752d3d8357c96f6
ENGLISH-LIGHT <2319513900@qq.com>
2018-04-23 17:30:28 +0800

    放在工程里面

--- grades management/server.js
+++ grades management/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 var app = express();
-app.listen(8080);
+app.listen(8888);
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+  res.end();
 });
 
